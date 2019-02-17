82a6cd18ee32ee876fab90bcad9a6aebac17fa8a
Florian CHAMPAUD <fchampaud@kyriopolis.fr>
2019-02-17 17:27:26 +0100

    clipper: make methods static, change arguments
    Make most arguments const

--- clipper.cpp
+++ clipper.cpp
@@ -2,33 +2,34 @@
 #include "clipper.h"
 
 std::vector<std::array<int, 2>>
-Clipping::Clipper::clip_polygon_to_window(std::vector<std::array<int, 2>> pl, std::vector<std::array<int, 2>> pw) {
+Clipping::Clipper::clip_polygon_to_window(const Polygon &polygon, const Polygon &pw) {
     std::vector<std::array<int, 2>> ps;
+    std::vector<std::array<int, 2>> pl {polygon.points};
     std::array<int, 2> s{};
     std::array<int, 2> f{};
 
-    for (int i = 0; i < pw.size(); ++i) {
+    for (int i = 0; i < pw.points.size(); ++i) {
         ps.clear();
 
         for (int j = 0; j < pl.size(); ++j) {
             if (j == 0) {
                 f = pl[j];
             } else {
-                if (cuts(s, pl[j], pw[i], pw[i+1])) {
-                    auto inter = intersect(s, pl[j], pw[i], pw[i + 1]);
+                if (cuts(s, pl[j], pw.points[i], pw.points[i+1])) {
+                    auto inter = intersect(s, pl[j], pw.points[i], pw.points[i + 1]);
                     ps.push_back(*inter);
                 }
             }
             s = pl[j];
 
-            if (visible(s, pw[i], pw[i+1])) {
+            if (visible(s, pw.points[i], pw.points[i+1])) {
                 ps.push_back(s);
             }
         }
 
         if (!ps.empty()) {
-            if (cuts(s, f, pw[i], pw[i+1])) {
-                auto inter = intersect(s, f, pw[i], pw[i+1]);
+            if (cuts(s, f, pw.points[i], pw.points[i+1])) {
+                auto inter = intersect(s, f, pw.points[i], pw.points[i+1]);
                 ps.push_back(*inter);
             }
 
@@ -40,17 +41,20 @@ Clipping::Clipper::clip_polygon_to_window(std::vector<std::array<int, 2>> pl, st
 }
 
 bool
-Clipping::Clipper::cuts(std::array<int, 2> &s1, std::array<int, 2> &e1, std::array<int, 2> &s2, std::array<int, 2> &e2) {
+Clipping::Clipper::cuts(const std::array<int, 2> &s1, const std::array<int, 2> &e1, const std::array<int, 2> &s2,
+                        const std::array<int, 2> &e2) {
     return false;
 }
 
-std::array<int, 2> *
-Clipping::Clipper::intersect(std::array<int, 2> &s1, std::array<int, 2> &e1, std::array<int, 2> &s2, std::array<int, 2> &e2) {
+const std::array<int, 2> *
+Clipping::Clipper::intersect(const std::array<int, 2> &s1, const std::array<int, 2> &e1, const std::array<int, 2> &s2,
+                             const std::array<int, 2> &e2) {
     return &s1;
 }
 
 bool
-Clipping::Clipper::visible(std::array<int, 2> &p, std::array<int, 2> &seg_start, std::array<int, 2> &seg_end) {
+Clipping::Clipper::visible(const std::array<int, 2> &p, const std::array<int, 2> &seg_start,
+                           const std::array<int, 2> &seg_end) {
     return false;
 }
 
--- clipper.h
+++ clipper.h
@@ -7,19 +7,21 @@
 
 #include <vector>
 #include <array>
+#include "Polygon.h"
 
 namespace Clipping {
     class Clipper {
     public:
-        std::vector<std::array<int, 2>> clip_polygon_to_window(std::vector<std::array<int, 2>>,
-                                                               std::vector<std::array<int, 2>>);
+        static std::vector<std::array<int, 2>> clip_polygon_to_window(const Clipping::Polygon &, const Polygon &);
 
-        bool cuts(std::array<int, 2> &s1, std::array<int, 2> &e1, std::array<int, 2> &s2, std::array<int, 2> &e2);
+        static bool cuts(const std::array<int, 2> &s1, const std::array<int, 2> &e1, const std::array<int, 2> &s2,
+                         const std::array<int, 2> &e2);
 
-        std::array<int, 2> *intersect(std::array<int, 2> &s1, std::array<int, 2> &e1, std::array<int, 2> &s2,
-                                      std::array<int, 2> &e2);
+        static const std::array<int, 2> * intersect(const std::array<int, 2> &s1, const std::array<int, 2> &e1,
+                                                    const std::array<int, 2> &s2, const std::array<int, 2> &e2);
 
-        bool visible(std::array<int, 2> &p, std::array<int, 2> &seg_start, std::array<int, 2> &seg_end);
+        static bool visible(const std::array<int, 2> &p, const std::array<int, 2> &seg_start,
+                            const std::array<int, 2> &seg_end);
 
         std::array<int, 2> *norm(std::array<int, 2> &x1, std::array<int, 2> &y1);
     };
